7b583b0737c3b9cfe2d4c7d57ce2699e33963d3b
prockenschaub <rockenschaub.patrick@gmail.com>
2022-10-27 11:46:15 +0200

    Define and investigate AKI task

---
+++ aki.R
@@ -0,0 +1,163 @@
+library(argparser)
+library(assertthat)
+library(rlang)
+library(data.table)
+library(vctrs)
+library(ricu)
+
+source("R/misc.R")
+source("R/steps.R")
+source("R/sequential.R")
+source("R/obs_time.R")
+
+# https://link.springer.com/article/10.1007/s00134-017-4678-3
+
+# Create a parser
+p <- arg_parser("Extract and preprocess ICU AKI data")
+p <- add_argument(p, "--src", help="source database", default="eicu_demo")
+argv <- parse_args(p)
+
+src <- argv$src 
+conf <- ricu:::read_json("config.json")
+path <- file.path(conf$output_dir, "aki")
+
+
+cncpt_env <- new.env()
+
+# Task description
+time_flow <- "sequential" # sequential / continuous
+time_unit <- hours
+freq <- 1L
+max_len <- 7 * 24  # = 7 days
+
+static_vars <- c("age", "sex", "height", "weight")
+
+dynamic_vars <- c("alb", "alp", "alt", "ast", "be", "bicar", "bili", "bili_dir",
+                  "bnd", "bun", "ca", "cai", "ck", "ckmb", "cl", "crea", "crp", 
+                  "dbp", "fgn", "fio2", "glu", "hgb", "hr", "inr_pt", "k", "lact",
+                  "lymph", "map", "mch", "mchc", "mcv", "methb", "mg", "na", "neut", 
+                  "o2sat", "pco2", "ph", "phos", "plt", "po2", "ptt", "resp", "sbp", 
+                  "temp", "tnt", "urine", "wbc")
+
+
+# cross-sectional vs longitudinal
+predictor_type <- "dynamic" # static / dynamic
+outcome_type   <- "dynamic" # static / dynamic
+
+
+patients <- stay_windows(src, interval = time_unit(freq))
+patients <- as_win_tbl(patients, index_var = "start", dur_var = "end", interval = time_unit(freq))
+
+
+# Define outcome ----------------------------------------------------------
+
+outc <- load_step(dict["aki"], cache = TRUE)
+outc <- summary_step(outc, "first")
+
+
+# Define observation times ------------------------------------------------
+
+stop_obs_at(outc, offset = hours(6L), by_ref = TRUE)
+stop_obs_at(patients, offset = ricu:::re_time(max_len, time_unit(freq)), by_ref = TRUE)
+
+
+# Apply exclusion criteria ------------------------------------------------
+
+# 1. Stay <6h
+x <- load_step("los_icu")
+x <- filter_step(x, ~ . < 6 / 24)
+
+excl1 <- unique(x[, id_vars(x), with = FALSE])
+
+
+# 2. Less than 4 measurements
+n_obs_per_row <- function(x, ...) {
+  # TODO: make sure this does not change by reference if a single concept is provided
+  obs <- data_vars(x)
+  x[, n := as.vector(rowSums(!is.na(.SD))), .SDcols = obs]
+  x[, .SD, .SDcols = !c(obs)]
+}
+
+x <- load_step(dict[dynamic_vars], cache = TRUE)
+x <- summary_step(x, "count", drop_index = TRUE)
+x <- filter_step(x, ~ . < 4)
+
+excl2 <- unique(x[, id_vars(x), with = FALSE])
+
+
+# 3. More than 12 hour gaps between measurements
+map_to_grid <- function(x) {
+  grid <- ricu::expand(patients)
+  merge(grid, x, all.x = TRUE)
+}
+
+longest_rle <- function(x, val) {
+  x <- x[, rle(.SD[[data_var(x)]]), by = c(id_vars(x))]
+  x <- x[values != val, lengths := 0]
+  x[, .(lengths = max(lengths)), , by = c(id_vars(x))]
+}
+
+x <- load_step(dict[dynamic_vars], cache = TRUE)
+x <- function_step(x, map_to_grid)
+x <- function_step(x, n_obs_per_row)
+x <- mutate_step(x, ~ . > 0)
+x <- function_step(x, longest_rle, val = FALSE)
+x <- filter_step(x, ~ . >= 12)
+
+excl3 <- unique(x[, id_vars(x), with = FALSE])
+
+
+# 4. Age < 18
+x <- load_step("age")
+x <- filter_step(x, ~ . < 18)
+
+excl4 <- unique(x[, id_vars(x), with = FALSE])
+
+
+# 5. AKI onset before ICU
+x1 <- load_step(dict["aki"], cache = TRUE)
+x2 <- summary_step(x1, "first")
+x3 <- filter_step(x2, ~ . < 0, col = index_col)
+
+excl5 <- unique(x3[, id_vars(x), with = FALSE])
+
+
+# Apply exclusions
+patients <- exclude(patients, mget(paste0("excl", 1:5)))
+attrition <- as.data.table(patients[c("incl_n", "excl_n_total", "excl_n")])
+patients <- patients[['incl']]
+patient_ids <- patients[, .SD, .SDcols = id_var(patients)]
+
+
+# Prepare data ------------------------------------------------------------
+
+# Get predictors
+dyn <- load_step(dict[dynamic_vars], cache = TRUE)
+sta <- load_step(dict[static_vars], cache = TRUE)
+
+# Transform all variables into the target format
+assert_that(outcome_type == "dynamic", time_flow == "sequential")
+
+outc_fmt <- function_step(outc, map_to_grid)
+outc_fmt <- function_step(outc_fmt, outcome_window, window = c(6L, 6L))
+rename_cols(outc_fmt, c("stay_id", "time", "label"), by_ref = TRUE)
+
+dyn_fmt <- function_step(dyn, map_to_grid)
+rename_cols(dyn_fmt, c("stay_id", "time"), meta_vars(dyn_fmt), by_ref = TRUE)
+
+sta_fmt <- sta[patient_ids]  # TODO: make into step
+rename_cols(sta_fmt, c("stay_id"), id_vars(sta), by_ref = TRUE)
+
+
+# Write to disk -----------------------------------------------------------
+
+out_path <- paste0(path, "/", src)
+
+if (!dir.exists(out_path)) {
+  dir.create(out_path, recursive = TRUE)
+}
+
+arrow::write_parquet(outc_fmt, paste0(out_path, "/outc.parquet"))
+arrow::write_parquet(dyn_fmt, paste0(out_path, "/dyn.parquet"))
+arrow::write_parquet(sta_fmt, paste0(out_path, "/sta.parquet"))
+fwrite(attrition, paste0(out_path, "/attrition.csv"))
